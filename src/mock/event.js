dd8bc03c85bae3f8546696fdfa5c55d504706952
Andrusha645 <truth20052006@yandex.ru>
2023-01-16 14:17:12 +0300

    3

--- src/mock/event.js
+++ src/mock/event.js
@@ -1,63 +1,135 @@
 import {getRandomArrayElement, getRandomPositiveInteger} from '../utils.js';
 
-const mockEvents = [
+const  points = [
   {
-    destination:{
-      id: 1,
-      description: 'Chamonix, is a beautiful city, a true asian pearl, with crowded streets.',
-      name: 'Chamonix',
-      pictures: [
-        {
-          src: `https://loremflickr.com/248/152?random=${getRandomPositiveInteger(1,5)}`,
-          description: 'Chamonix parliament building'
-        }
-      ]
-    },
-    OffersByType:{
-      type: 'taxi',
+    id: 1,
+    description:'Chamonix-Mont-Blanc (usually shortened to Chamonix) is a resort area near the junction of France, Switzerland and Italy. At the base of Mont Blanc, the highest summit in the Alps, it is renowned for its skiing.',
+    name: 'Chamonix',
+    pictures: [
+      {
+        src: `https://loremflickr.com/248/152?random=${getRandomPositiveInteger(1,5)}`,
+        description: 'Chamonix parliament building'
+      }
+    ] 
+  },
+  {
+    id: 2,
+    description:'Geneva is a city in Switzerland that lies at the southern tip of expansive Lac Léman (Lake Geneva). Surrounded by the Alps and Jura mountains, the city has views of dramatic Mont Blanc.',
+    name: 'Geneva',
+    pictures: [
+      {
+        src: `https://loremflickr.com/248/152?random=${getRandomPositiveInteger(1,5)}`,
+        description: 'Geneva parliament building'
+      }
+    ] 
+  },
+];
+const offers = [
+  {
+    type: 'taxi',
       offers: [
         {
           'id': 1,
           'title': ' Choose the radio station',
           'price': 60
-        }
-      ]
-    },
-
-    basePrice: 1100,
-    dateFrom: '2019-07-10T22:55:56.845Z',
-    dateTo: '2019-07-11T11:22:13.375Z',
-
-  },
-  {
-    destination:{
-      id: 2,
-      description: 'Chamonix, is a beautiful city, a true asian pearl, with crowded streets.',
-      name: 'Chamonix',
-      pictures: [
-        {
-          src: `https://loremflickr.com/248/152?random=${getRandomPositiveInteger(1,5)}`,
-          description: 'Chamonix parliament building'
-        }
-      ]
-    },
-    OffersByType:{
-      type: 'flight',
-      offers: [
+        },
         {
           'id': 2,
-          'title': 'Upgrade to a business class',
-          'price': 120
+          'title': 'Order Uber',
+          'price': 20
         }
       ]
-    },
+  },
+  {
+    type: 'drive',
+      offers: [
+        {
+          'id': 1,
+          'title': 'Rent a car',
+          'price': 200
+        }
+      ]
+  },
+  {
+    type: 'check-in',
+      offers: [
+        {
+          'id': 1,
+          'title': 'Add breakfast',
+          'price': 50
+        },       
+      ]
+  },
+  {
+    type: 'sightseeing',
+      offers: [
+        {
+          'id': 1,
+          'title': 'Book tickets',
+          'price': 40
+        },
+        {
+          'id': 2,
+          'title': 'Lunch in city',
+          'price': 30
+        }
+      ]
+  },
+  {
+    type: 'flight',
+      offers: [
+        {
+          'id': 1,
+          'title': 'Add luggage',
+          'price': 50
+        },
+        {
+          'id': 2,
+          'title': 'Switch to comfort',
+          'price': 80
+        },
+        {
+          'id': 3,
+          'title': 'Add meal',
+          'price': 15
+        },
+        {
+          'id': 4,
+          'title': 'Choose seats',
+          'price': 5
+        },
+        {
+          'id': 5,
+          'title': 'Travel by train',
+          'price': 40
+        },
 
-    basePrice: 800,
+      ]
+  },
+  {
+
+  },
+];
+const mockEvents = [
+  { 
+    date: 'Mar19',
+    type: 'taxi',
+    cityName: 'Amsterdam',    
     dateFrom: '2019-07-10T22:55:56.845Z',
     dateTo: '2019-07-11T11:22:13.375Z',
-
-  }
-
+    basePrice: 80,
+    offers: ''
+  },
+  { 
+    date: 'Mar18',
+    type: 'check-in',
+    cityName: 'Geneva',    
+    dateFrom: '2019-06-10T22:55:56.845Z',
+    dateTo: '2019-06-11T11:22:13.375Z',
+    basePrice: 1100,
+    offers: ''
+  },
+  
 
 ];
 
